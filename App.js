f8db852e3c36aada0cbfbd1f1cdd3d98658f7128
chinmay <chinmaymhatre12@gmail.com>
2021-11-07 10:00:28 +0530

    add components

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, Image, TouchableOpacity, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is a text field</Text>
+      {/* Add one more text field */}
+      <Button
+        title="Press me"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+      {/* Add a button */}
+      <Image
+        source={require('./assets/favicon.png')}
+        />
+      {/* Add an image using splash.png */}
+      <TouchableOpacity onPress={() => Alert.alert('You tapped the button!')}>
+        <Text>TouchableOpacity</Text>
+      </TouchableOpacity>
+      {/* Add another touchable */}
+
+      <TextInput
+        style={styles.input}
+      />
+      {/* Add a text input */}
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +36,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    width: 200,
+    padding: 10,
   },
 });
